4164c7eac847cc4d9dbfc6cc02c48ce526f4d836
Aditya-singh101 <vishusingh212301@gmail.com>
2025-08-28 17:04:25 +0530

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import './App.css'
+import {Navbar} from './components/Navbar'
+import {Hero} from './components/Hero'
+import {Projects} from './components/Projects'
+import {Contact} from './components/Contact'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
 
+  useEffect(() => {
+      setIsLoaded(true);
+  }, [])
 
   return (
-    <>
-  
-    </>
+    <div className = {`app ${isLoaded ? "loaded" : ""}`}>
+      <Navbar/>
+
+      <Hero/>
+
+      <Projects/>
+
+      <Contact/>
+
+      <>
+        <p> &copy; 2025 Aditya All rights reserved.  </p>
+      </>
+
+
+    </div>
   )
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import {motion} from "framer-motion";
+
+export const Hero = () => {
+  return <motion.section id = "home" className="hero" initial={{opacity:0}}>
+    <div className="hero-container">
+
+    </div>
+  </motion.section>
+};
